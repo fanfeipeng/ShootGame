9777a91f8b12d5503b255c88165bb31207539998
youmi <s1092806@gm.pu.edu.tw>
2022-05-31 02:36:00 +0800

    v.5

--- app/src/main/java/tw/edu/pu/s1092806/shootgame/Fly.kt
+++ app/src/main/java/tw/edu/pu/s1092806/shootgame/Fly.kt
@@ -5,9 +5,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Rect
-
 class Fly (context: Context) {
-
     val res = context.resources  //讀取資源
     var x:Int = 0
     var y:Int = res.displayMetrics.heightPixels/2
@@ -17,6 +15,7 @@ class Fly (context: Context) {
     var SrcRect: Rect
     lateinit var DestRect: Rect
     var count : Int = 1
+    var fire : Int = 0
 
     init {
         image = BitmapFactory.decodeResource(res, R.drawable.fly1)
@@ -25,20 +24,36 @@ class Fly (context: Context) {
         h = image.height/4
         y -= h/2
     }
-
     fun draw(canvas: Canvas) {
         DestRect = Rect(x, y, x + w, y + h)
         canvas.drawBitmap(image, SrcRect, DestRect, null)
     }
 
     fun update(){
-        if (count==1){
-            count = 2
-            image = BitmapFactory.decodeResource(res, R.drawable.fly2)
-        }
-        else{
-            count = 1
-            image = BitmapFactory.decodeResource(res, R.drawable.fly1)
+
+            if (fire == 0){
+                if (count==1){
+                    count = 2
+                    image = BitmapFactory.decodeResource(res, R.drawable.fly2)
+                }
+                else{
+                    count = 1
+                    image = BitmapFactory.decodeResource(res, R.drawable.fly1)
+                }
+            }
+            else{
+
+                when(fire){
+                    1 -> image = BitmapFactory.decodeResource(res, R.drawable.shoot1)
+                    2 -> image = BitmapFactory.decodeResource(res, R.drawable.shoot2)
+                    3 -> image = BitmapFactory.decodeResource(res, R.drawable.shoot3)
+                    4 -> image = BitmapFactory.decodeResource(res, R.drawable.shoot4)
+                    5 -> image = BitmapFactory.decodeResource(res, R.drawable.shoot5)
+                }
+                fire++
+                if (fire>5){
+                    fire = 0
+                }
+            }
         }
     }
-}
